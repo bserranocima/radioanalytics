95d7dee129d151b0cda9f35871bd433f1520eff1
Brayan Serrano <bserrano@CimacastDevs-iMac.local>
2018-01-11 16:05:01 -0500

    Prod config updated

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -1849,9 +1849,9 @@ var UserService = (function () {
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
 var environment = {
     production: true,
-    siteUrl: '//52.202.179.22:3000',
-    apiUrl: '//52.202.179.22:3000',
-    socketIOUrl: '//52.202.179.22:3000'
+    siteUrl: '//radiostats.4cloud.co',
+    apiUrl: '//radiostats.4cloud.co',
+    socketIOUrl: '//radiostats.4cloud.co'
 };
 
 
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  siteUrl: '//52.202.179.22:3000',
-  apiUrl: '//52.202.179.22:3000',
-  socketIOUrl: '//52.202.179.22:3000'
+  siteUrl: '//radiostats.4cloud.co',
+  apiUrl: '//radiostats.4cloud.co',
+  socketIOUrl: '//radiostats.4cloud.co'
 };
